b31b36e91f84cbb8cb41f95b569a4bb90f4cb132
Helen Yang <helen.xu.yang@gmail.com>
2024-02-07 17:16:26 -0600

    Initial setup for geolocation

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+import { GeolocationChecker } from './GeolocationChecker'
+
+function App() {
+
+  return (
+    <GeolocationChecker />
+  )
+}
+
+export default App
---
+++ src/GeolocationChecker.tsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react";
+import { BoundedCoords, hardingBandBuildingCoords, testCoords } from "./locationData";
+
+const isPositionInBoundary = (currentPosition: GeolocationPosition, boundary: BoundedCoords) => {
+  const { latitude, longitude } = currentPosition.coords;
+  const { northLatitude, southLatitude, westLongitude, eastLongitude } = boundary.bounds;
+  if (latitude <= northLatitude
+    && latitude >= southLatitude
+    && longitude >= westLongitude
+    && longitude <= eastLongitude) {
+    return true;
+  }
+  return false;
+}
+
+export const GeolocationChecker = () => {
+
+  const [location, setLocation] = useState<GeolocationPosition | null>(null);
+  const geolocation = navigator.geolocation;
+
+  useEffect(() => {
+    geolocation.getCurrentPosition(onSuccess, onError, {
+      enableHighAccuracy: true, maximumAge: 30000,
+      timeout: 27000,
+    });
+  }, []);
+
+  const onSuccess = (position: GeolocationPosition) => {
+    setLocation(position);
+    console.log(position);
+  }
+
+  const onError = (error: GeolocationPositionError) => {
+    console.log('Error getting location:', error);
+  }
+
+
+  if (!location) {
+    return <p>Unknown location</p>
+  }
+
+  const { latitude, longitude } = location.coords;
+  return <div>
+    <p>Your location:</p>
+    <p>{`(${latitude}, ${longitude})`}</p>
+    <p>Are you at the Band Building? {isPositionInBoundary(location, hardingBandBuildingCoords) ? 'Yes' : 'No'}</p>
+    <p>Are you at test location? {isPositionInBoundary(location, testCoords) ? 'Yes' : 'No'}</p>
+  </div>
+}
